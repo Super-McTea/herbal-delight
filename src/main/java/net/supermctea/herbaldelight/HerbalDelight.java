1cee76fe32eee783901600869d01a3d63330a4ef
Super-McTea <calebjcruz@icloud.com>
2024-02-29 19:41:41 +1100

    Right before any progress
    Come back here for new mods?

--- src/main/java/net/supermctea/herbaldelight/HerbalDelight.java
+++ src/main/java/net/supermctea/herbaldelight/HerbalDelight.java
@@ -12,6 +12,6 @@ public class HerbalDelight implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Fabric world! Ready to begin?");
 	}
 }
