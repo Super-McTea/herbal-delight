ddbf15743d3d04d2d51b1c597cfcb7943963c0fe
Super-McTea <calebjcruz@icloud.com>
2024-02-29 19:37:28 +1100

    Initial commit

---
+++ src/main/java/net/supermctea/herbaldelight/HerbalDelight.java
@@ -0,0 +1,17 @@
+package net.supermctea.herbaldelight;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HerbalDelight implements ModInitializer {
+	public static final String MOD_ID = "herbaldelight";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/supermctea/herbaldelight/HerbalDelightClient.java
@@ -0,0 +1,10 @@
+package net.supermctea.herbaldelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HerbalDelightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
