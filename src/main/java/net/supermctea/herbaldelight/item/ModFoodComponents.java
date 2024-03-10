8d824914f9f52496a02f56fdc3f4a14bce0b9cbb
Super-McTea <calebjcruz@icloud.com>
2024-03-11 03:28:16 +1100

    The mod is finished

--- src/main/java/net/supermctea/herbaldelight/item/ModFoodComponents.java
+++ src/main/java/net/supermctea/herbaldelight/item/ModFoodComponents.java
@@ -6,12 +6,12 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent GREEN_TEA_CUP = new FoodComponent.Builder().hunger(3).saturationModifier(0.2f)
-            .snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 300), 0.3f)
+            .snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 300), 0.5f)
             .build();
     public static final FoodComponent BLACK_TEA_CUP = new FoodComponent.Builder().hunger(4).saturationModifier(0.3f)
-            .snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400), 0.3f)
+            .snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400), 0.5f)
             .build();
     public static final FoodComponent HIBISCUS_TEA_CUP = new FoodComponent.Builder().hunger(3).saturationModifier(0.2f)
-            .snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 300), 0.3f)
+            .snack().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 300), 0.5f)
             .build();
 }
